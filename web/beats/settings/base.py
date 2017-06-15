49c52d1e74b4d8a2897c9beac3d3e7289ca6aebe
fourwills <action.worker519@gmail.com>
2017-06-15 15:48:33 -0600

    Init nginx as a reverse proxy

--- web/beats/settings/base.py
+++ web/beats/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h9v)-dt$mf&b7nsv5qrt3b08-%c16+&z-ezd5a(jv1!cq4_!%_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
