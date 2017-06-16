ff41ea1e617e509e83977317f0b05bce628fbd3f
fourwills <action.worker519@gmail.com>
2017-06-16 16:25:31 -0600

    Create API Serializers

---
+++ web/beats/apps/tracks/api/v1/serializers.py
@@ -0,0 +1,35 @@
+""" Serializers for Tracks APIs """
+
+from rest_framework import serializers
+
+from ...models import Genre, Track
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """ Serializer for Model Genre """
+
+    class Meta:
+        """ Meta Genre Serializer """
+        model = Genre
+        fields = ('id', 'name')
+        read_only = 'id'
+
+class TrackSerializer(serializers.ModelSerializer):
+    """ Serializer for generic Model Track data """
+
+    class Meta:
+        """ Meta Track Serializer """
+        model = Track
+        fields = ('id', 'title', 'genre', 'release_date', 'is_purchasable', 'purchase_price')
+
+
+class TrackDetailSerializer(serializers.ModelSerializer):
+    """ Serializer for detailed Model Track data """
+
+    class Meta:
+        """ Meta Track Detail Serializer """
+        model = Track
+        fields = (
+            'id', 'title', 'genre', 'release_date', 'is_purchasable',
+            'purchase_price', 'is_available_for_mix', 'is_active',
+            'date_created', 'date_updated')
