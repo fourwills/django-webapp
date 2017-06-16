b8b180c1be1b99365c56f7d9185766defad9372a
fourwills <action.worker519@gmail.com>
2017-06-16 16:23:44 -0600

    Fix pylint errors

--- web/beats/apps/tracks/urls.py
+++ web/beats/apps/tracks/urls.py
@@ -1,7 +1,8 @@
+""" URL routes for Tracks non-API views """
 from django.conf.urls import url
 
 from .views import IndexView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index')
+    url(r'^$', IndexView.as_view(), name='index'),
 ]
