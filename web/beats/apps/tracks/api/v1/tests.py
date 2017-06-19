9967e0436b8dc7a2e918082fbb07f3fb52e43fa7
fourwills <action.worker519@gmail.com>
2017-06-19 09:58:03 -0600

    API endpoints for track list and track detailed, and unit tests

--- web/beats/apps/tracks/api/v1/tests.py
+++ web/beats/apps/tracks/api/v1/tests.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 from django.core.urlresolvers import reverse
 from rest_framework import status
@@ -13,88 +14,43 @@ class TracksListTestCase(APITestCase):
         """ Set Up """
         self.client = APIClient()
 
-    def tearDown(self):
-        """ Clean up """
-        Genre.objects.all().delete()
-        Track.objects.all().delete()
-
-    def test_get_track_list_empty_items(self):
-        """ Test API for blank track list """
+    def test_get_track_list(self):
+        """ Test API for track list, returns only basic info (name) """
         url = reverse('api:tracks:track-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, [])
+        data = json.loads(response.data)
 
-    def test_get_track_list_with_items(self):
-        """ Test API for blank track list """
-
-        # Test one object appears in list
-        genre_obj = Genre.objects.create(name='house')
-        Track.objects.create(
-            release_date=datetime.now(),
-            purchase_price=10.75,
-            genre=genre_obj,
-            title="First Here"
-        )
-        url = reverse('api:tracks:track-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], "First Here")
-        self.assertEqual(response.data[0]['purchase_price'], '10.75')
-
-        # Test a second object appears in list
-        Track.objects.create(
-            release_date=datetime.now(),
-            purchase_price=11,
-            genre=genre_obj,
-            title="Second Here"
-        )
-        url = reverse('api:tracks:track-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 2)
-        self.assertEqual(response.data[1]['title'], "Second Here")
-        self.assertEqual(response.data[1]['purchase_price'], '11.00')
-
-
+        self.assertGreater(len(data), 0)
+        get_key = data[0].get('name', None)
+        self.assertIsNotNone(get_key)
+        self.assertRaises(KeyError, lambda: data[0]['sku'])
+        self.assertRaises(KeyError, lambda: data[0]['purchasable'])
+        
 class TracksDetailTestCase(APITestCase):
     """ Test Track API by specific PK, returns detailed info """
 
     def setUp(self):
         self.client = APIClient()
 
-    def tearDown(self):
-        Genre.objects.all().delete()
-        Track.objects.all().delete()
-
     def test_get_track_by_nonexistent_pk(self):
         """ Track not found should return 404 """
-        nonexistent_pk = 999  # Should have been cleaned up from test tearDown()
+        nonexistent_pk = 0
         url = reverse('api:tracks:track-detail', kwargs={'pk': nonexistent_pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.data['detail'], 'Not found.')
+        self.assertEqual(response.json(), [])
 
     def test_get_track_by_existing_pk(self):
         """ Track found should return 200 with details """
-        genre_obj = Genre.objects.create(name='house')
-        Track.objects.create(
-            release_date=datetime.now(),
-            purchase_price=10.75,
-            genre=genre_obj,
-            title="I am here!"
-        )
-        track_obj = Track.objects.first()
-        existing_pk = track_obj.pk
+        existing_pk = 8650404
         url = reverse('api:tracks:track-detail', kwargs={'pk': existing_pk})
         response = self.client.get(url)
-
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['title'], 'I am here!')
-        self.assertEqual(response.data['is_available_for_mix'], False)
-        self.assertEqual(response.data['is_purchasable'], False)
-
+        self.assertEqual(response.data['purchasable'], True)
+        self.assertEqual(response.data['type'], 'track')
+        self.assertEqual(response.data['sku'], 'track-8650404')
+        
 class FileUploadTestCase(APITestCase):
     """ Upload File to Media dir """
 
@@ -102,7 +58,10 @@ class FileUploadTestCase(APITestCase):
         self.client = APIClient()
 
     def tearDown(self):
-        FileUpload.objects.all().delete() 
+        file_query = FileUpload.objects.all()
+        for file_obj in file_query:
+            if file_obj:
+                file_obj.doc.delete()
 
     def test_put_invalid_data(self):
         url = reverse('api:tracks:track-upload')
--- web/beats/apps/tracks/api/v1/views.py
+++ web/beats/apps/tracks/api/v1/views.py
@@ -1,11 +1,18 @@
 """ API Views for Tracks app """
+import json
+
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import FileUploadParser, MultiPartParser
-from django.conf import settings
+import requests
+from requests_oauthlib import OAuth1
 
+from beats.settings.env import (
+    OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
+)
 
 from ...models import Track, FileUpload
 from ...forms import FileUploadForm 
@@ -13,17 +20,33 @@ from .serializers import (
     TrackSerializer, TrackDetailSerializer, FileUploadSerializer
 )
 
-class TrackListAPI(generics.ListAPIView):
+class TrackListAPI(APIView):
     """ API endpoint for listing all tracks """
-    queryset = Track.objects.all()
-    serializer_class = TrackSerializer
+    auth = OAuth1(
+        OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET,
+        OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    tracks_url = 'https://oauth-api.beatport.com/catalog/3/tracks'
+    def get(self, request, *args, **kwargs):
+        bp_response = requests.get(self.tracks_url, auth=self.auth)
+        full_results = bp_response.json()['results']
+        filtered_data = []
+        for track in full_results:
+            filtered_data.append({'name': track['name']})
+        return Response(json.dumps(filtered_data), status=bp_response.status_code)
 
 class TrackDetailAPI(generics.RetrieveAPIView):
     """ API endpoint for GET on a single track """
-    serializer_class = TrackDetailSerializer
+    auth = OAuth1(
+        OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET,
+        OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    tracks_url = 'https://oauth-api.beatport.com/catalog/3/tracks?id='
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        bp_response = requests.get(self.tracks_url + pk, auth=self.auth)
+        if len(bp_response.json()['results']) != 1:
+            return Response(bp_response.json()['results'], status=status.HTTP_404_NOT_FOUND)
+        return Response(bp_response.json()['results'][0], status=bp_response.status_code)
 
-    def get_object(self):
-        return get_object_or_404(Track, pk=self.kwargs['pk'])
 
 class TrackUploadAPI(APIView):
     """ API endpoint for PUT to upload a file """
